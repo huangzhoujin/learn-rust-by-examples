1bcef63c40ecc044b90e1e14d2101fa4b3e19670
huangzhoujin <porkin@vip.163.com>
2024-11-16 09:11:04 +0800

    1.2.1. 调试（debug）

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,56 @@
 /*
-  format!：将格式化文本写到字符串。
-  print!：与 format! 类似，但将文本输出到控制台（io::stdout）。
-  println!: 与 print! 类似，但输出结果追加一个换行符。
-  eprint!：与 print! 类似，但将文本输出到标准错误（io::stderr）。
-  eprintln!：与 eprint! 类似，但输出结果追加一个换行符。
-  fmt::Debug：使用 {:?} 标记。格式化文本以供调试使用。
-  fmt::Display：使用 {} 标记。以更优雅和友好的风格来格式化文本
+  fmt::Debug 这个 trait 使这项工作变得相当简单。所有类型都能推导（derive，即自动创建）
+  fmt::Debug 的实现。但是 fmt::Display 需要手动实现。
  */
+use std::fmt;
+// 这个结构体不能使用 `fmt::Display` 或 `fmt::Debug` 来进行打印。
+struct UnPrintable(i32);
+
+// `derive` 属性会自动创建所需的实现，使这个 `struct` 能使用 `fmt::Debug` 打印。
+#[derive(Debug)]
+struct DebugPrintable(i32);
+
+/// 推导 `Structure` 的 `fmt::Debug` 实现。
+// `Structure` 是一个包含单个 `i32` 的结构体。
+#[derive(Debug)]
+struct Structure(i32);
+
+// 将 `Structure` 放到结构体 `Deep` 中。然后使 `Deep` 也能够打印。
+#[derive(Debug)]
+struct Deep(Structure);
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
+// 通过手动实现 fmt::Display 来控制显示效果。
+impl fmt::Display for Person<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}[{}]", self.name, self.age)
+    }
+}
+
 fn main() {
-    // 通常情况下，`{}` 会被任意变量内容所替换。
-    // 变量内容会转化成字符串。
-    println!("{} days", 31);
+    // 使用 `{:?}` 打印和使用 `{}` 类似。
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
 
-    // 不加后缀的话，31 就自动成为 i32 类型。
-    // 你可以添加后缀来改变 31 的类型（例如使用 31i64 声明 31 为 i64 类型）。
+    // `Structure` 也可以打印！
+    println!("Now {:?} will print!", Structure(3));
 
-    // 用变量替换字符串有多种写法。
-    // 比如可以使用位置参数。
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    // 使用 `derive` 的一个问题是不能控制输出的形式。
+    // 假如我只想展示一个 `7` 怎么办？
+    println!("Now {:?} will print!", Deep(Structure(7)));
 
-    // 可以使用命名参数。
-    println!("{subject} {verb} {object}",
-             object="the lazy dog",
-             subject="the quick brown fox",
-             verb="jumps over");
-
-    // 可以在 `:` 后面指定特殊的格式。
-    println!("{} of {:b} people know binary, the other half don't", 1, 2);
-
-    // 你可以按指定宽度来右对齐文本。
-    // 下面语句输出 "     1"，5 个空格后面连着 1。
-    println!("{number:>width$}", number=1, width=6);
-
-    // 你可以在数字左边补 0。下面语句输出 "000001"。
-    println!("{number:>0width$}", number=1, width=6);
-
-    // println! 会检查使用到的参数数量是否正确。
-    println!("My name is {0}, {1} {0}", "Bond", "James");
-    // 改正 ^ 补上漏掉的参数："James"
-
-    // 创建一个包含单个 `i32` 的结构体（structure）。命名为 `Structure`。
-    #[allow(dead_code)]
-    struct Structure(i32);
-
-    // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
-    // 下面语句无法运行。
-    // println!("This struct `{}` won't print...", Structure(3));
-    // 改正 ^ 注释掉此行。
-    let pi = 3.141592;
-    println!("Pi is roughly {:.3}", pi);
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+    // 美化打印
+    println!("{:#?}", peter);
+    println!("{}", peter);
 }
