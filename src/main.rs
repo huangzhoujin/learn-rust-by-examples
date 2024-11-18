01cf8be0fa053d2eaf29a0f3f571c77025734de2
Huang Zhoujin <huangzhoujin@hotmail.com>
2024-11-18 10:15:04 +0800

    1.2.4. 格式化

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,69 @@
 /*
- Rust 专门为解决这个问题提供了 ? 操作符。
+格式化的方式是通过格式字符串来指定的：
+  format!("{}", foo) -> "3735928559"
+  format!("0x{:X}", foo) -> "0xDEADBEEF"
+  format!("0o{:o}", foo) -> "0o33653337357"
  */
-use std::fmt; // 导入 `fmt` 模块。
+use std::fmt::{self, Formatter, Display};
 
-// 定义一个包含单个 `Vec` 的结构体 `List`。
-struct List(Vec<i32>);
+struct City {
+    name: &'static str,
+    // 纬度
+    lat: f32,
+    // 经度
+    lon: f32,
+}
 
-impl fmt::Display for List {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // 使用元组的下标获取值，并创建一个 `vec` 的引用。
-        let vec = &self.0;
+impl Display for City {
+    // `f` 是一个缓冲区（buffer），此方法必须将格式化后的字符串写入其中
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
 
-        write!(f, "[")?;
+        // `write!` 和 `format!` 类似，但它会将格式化后的字符串写入
+        // 一个缓冲区（即第一个参数f）中。
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
 
-        // 使用 `v` 对 `vec` 进行迭代，并用 `count` 记录迭代次数。
-        for (count, v) in vec.iter().enumerate() {
-            // 对每个元素（第一个元素除外）加上逗号。
-            // 使用 `?` 或 `try!` 来返回错误。
-            if count != 0 { write!(f, ", ")?; }
-            /*
-              更改程序使 vector 里面每个元素的下标也能够打印出来。新的结果如下：
-              [0: 1, 1: 2, 2: 3]
-            */
-            write!(f, "{} : {}", count, v)?;
-        }
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
-        // 加上配对中括号，并返回一个 fmt::Result 值。
-        write!(f, "]")
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "RGB ({}, {}, {}) 0x{:02X}{:02X}{:02X}",
+               self.red, self.green, self.blue, self.red, self.green, self.blue)
     }
 }
 
 fn main() {
-    let v = List(vec![1, 2, 3]);
-    println!("{}", v);
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{:?}", *color);
+    }
 
-    let v2 = List(vec![]);
-    println!("{}", v2);
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{}", *color);
+    }
 }
