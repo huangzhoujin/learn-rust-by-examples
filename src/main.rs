1c933592b3be9ff1cc875b6085c236d0af4e563d
Huang Zhoujin <huangzhoujin@hotmail.com>
2024-11-18 09:56:38 +0800

    1.2.3. 测试实例：List

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,39 @@
 /*
-  fmt::Display 采用 {} 标记。
+ Rust 专门为解决这个问题提供了 ? 操作符。
  */
-// （使用 `use`）导入 `fmt` 模块使 `fmt::Display` 可用
-// （使用 `use`）导入 `fmt` 模块使 `fmt::Display` 可用
-use std::fmt; // 导入 `fmt`
+use std::fmt; // 导入 `fmt` 模块。
 
-// 定义一个结构体，咱们会为它实现 `fmt::Display`。以下是个简单的元组结构体
-// `Structure`，包含一个 `i32` 元素。
-struct Structure(i32);
+// 定义一个包含单个 `Vec` 的结构体 `List`。
+struct List(Vec<i32>);
 
-// 为了使用 `{}` 标记，必须手动为类型实现 `fmt::Display` trait。
-impl fmt::Display for Structure {
-    // 这个 trait 要求 `fmt` 使用与下面的函数完全一致的函数签名
+impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // 仅将 self 的第一个元素写入到给定的输出流 `f`。返回 `fmt:Result`，此
-        // 结果表明操作成功或失败。注意 `write!` 的用法和 `println!` 很相似。
-        write!(f, "{}", self.0)
-    }
-}
+        // 使用元组的下标获取值，并创建一个 `vec` 的引用。
+        let vec = &self.0;
 
-// 带有两个数字的结构体。推导出 `Debug`，以便与 `Display` 的输出进行比较。
-#[derive(Debug)]
-struct MinMax(i64, i64);
+        write!(f, "[")?;
 
-// 实现 `MinMax` 的 `Display`。
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // 使用 `self.number` 来表示各个数据。
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
-
-// 为了比较，定义一个含有具名字段的结构体。
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
-
-// 类似地对 `Point2D` 实现 `Display`
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // 自定义格式，使得仅显示 `x` 和 `y` 的值。
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
-}
-
-#[derive(Debug)]
-struct Complex  {
-    real: f64,
-    imag: f64,
-}
-
-impl fmt::Display for Complex {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if(self.imag < 0.0) {
-            write!(f, "{} - {}i", self.real, -self.imag)
-        } else {
-            write!(f, "{} + {}i", self.real, self.imag)
+        // 使用 `v` 对 `vec` 进行迭代，并用 `count` 记录迭代次数。
+        for (count, v) in vec.iter().enumerate() {
+            // 对每个元素（第一个元素除外）加上逗号。
+            // 使用 `?` 或 `try!` 来返回错误。
+            if count != 0 { write!(f, ", ")?; }
+            /*
+              更改程序使 vector 里面每个元素的下标也能够打印出来。新的结果如下：
+              [0: 1, 1: 2, 2: 3]
+            */
+            write!(f, "{} : {}", count, v)?;
         }
+
+        // 加上配对中括号，并返回一个 fmt::Result 值。
+        write!(f, "]")
     }
 }
 
 fn main() {
-    let minmax = MinMax(0, 14);
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 
-    println!("Compare structures:");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range =   MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-
-    println!("The big range is {big} and the small is {small}",
-             small = small_range,
-             big = big_range);
-
-    let point = Point2D { x: 3.3, y: 7.2 };
-
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
-
-    // 仿照 Point2D 结构体增加一个名为 Complex 的结构体
-    let c1 = Complex { real: 3.1, imag: 7.2 };
-    let c2 = Complex { real: 3.1, imag: -7.2 };
-    println!("Complex c1:");
-    println!("Display: {}", c1);
-    println!("Debug: {:?}", c1);
-
-    println!("Complex c2:");
-    println!("Display: {}", c2);
-    println!("Debug: {:?}", c2);
+    let v2 = List(vec![]);
+    println!("{}", v2);
 }
